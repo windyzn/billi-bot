8143c5cd1509b18b2bcb01a3ff54a82e64f32e8b
windy <windyzn@users.noreply.github.com>
2026-05-04 16:52:18 -0700

    feat: Initialize new bill items with no shared users
    When adding a new bill item, it should not be automatically shared with all friends. This change sets the `sharedWith` array to empty by default, requiring explicit selection of who shares the item.

--- App.tsx
+++ App.tsx
@@ -111,7 +111,7 @@ const App: React.FC = () => {
       name,
       price,
       taxCategory,
-      sharedWith: friends.map(f => f.id),
+      sharedWith: [],
       isTaxIncluded,
       customTaxRate
     };
